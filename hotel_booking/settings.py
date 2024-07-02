ea5dd700bc7d0b212c4f1d49792fa85c5ae03b10
daniilmir3105 <miroshnichienko.daniil@gmail.com>
2024-07-02 11:31:01 +0300

    delete secret key

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0*!6br2^oa)r_3^2&b=#6xq2sb!)v!-2&ii^7#!kmvg$7c7@+'
+SECRET_KEY = '*****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
